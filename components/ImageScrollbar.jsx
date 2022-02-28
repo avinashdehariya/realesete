c7bdcd72dedf40dfaf79d0d055b70c1c0f8cdacb
avinashdehariya <avinasdehariyaji@gmail.com>
2022-02-28 10:59:49 +0530

    fouth commit give key props in and image to alt

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -41,19 +41,15 @@ export default function ImageScrollbar({ data }) {
       LeftArrow={LeftArrow}
       RightArrow={RightArrow}
       style={{ overflow: "hidden" }}
+      key={item.id}
     >
       {data.map((item) => (
-        <Box
-          width="910px"
-          key={item.id}
-          itemId={item.id}
-          overflow="hidden"
-          p="1"
-        >
+        <Box width="910px" key={item.id} overflow="hidden" p="1">
           <Image
             placeholder="blur"
             blurDataURL={item.url}
             src={item.url}
+            alt={item.name}
             width={1000}
             height={500}
             sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
